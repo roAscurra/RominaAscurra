92ae70e912a7b0fb0aa70a6b4d3838f80cb6410c
roAscurra <rominaascurra4887@gmail.com>
2024-02-08 20:45:56 -0300

    Router

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react"
+
+const Card = () => {
+    // //hook
+    // const [valor,setValor] = useState("")
+
+    // useEffect(() => {
+
+    // },[valor])
+
+    return(
+        <div>
+            <h6>Hola</h6>
+        </div>
+    )
+}
+
+export default Card
